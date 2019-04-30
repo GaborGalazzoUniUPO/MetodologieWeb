7edb149d7cf8fbd1ac80f0b70ee959e6220315a9
Gabor Galazzo <gabor.galazzo@star-group.net>
2019-04-30 16:07:06 +0200

    used position type and used reference for gameMatrix not CONST

--- Esercizio 1 Fifteen Puzzle (da consegnare)-20190423/fifteen.js
+++ Esercizio 1 Fifteen Puzzle (da consegnare)-20190423/fifteen.js
@@ -1,6 +1,8 @@
 (function () {
     'use strict';
 
+
+
     // Construction CONST
     const PUZZLE_DIM = 100;
     const NUM_COL = 4;
@@ -11,17 +13,16 @@
     const NUM_SHUFFLE = 100;
     const RANDOM_VALUE = 2;
 
-    //Engine const
-    const GAME_MATRIX = [GRID_DIM];
 
     /**
      * Return if the index piece of he gameMatrix can be moved
-     * @param {indexber} index The index of the piece in gameMatrix
+     * @param {Position[]} gameMatrix The array containing pieces position
+     * @param {index} index The index of the piece in gameMatrix
      * @returns {boolean} the piece can be moved
      */
-    function canMove(index) {
-        let selectedPosition = GAME_MATRIX[index];
-        let emptyPosition = GAME_MATRIX[GRID_DIM - 1];
+    function canMove(gameMatrix, index) {
+        let selectedPosition = gameMatrix[index];
+        let emptyPosition = gameMatrix[GRID_DIM - 1];
         if (selectedPosition.x === emptyPosition.x) {
             return Math.abs(selectedPosition.y - emptyPosition.y) === 1;
         }
@@ -32,16 +33,17 @@
     }
 
     /**
-     * Mve a piece of the puzzle if it can be moved
+     * Move a piece of the puzzle if it can be moved
+     * @param {Position[]} gameMatrix The array containing pieces position
      * @param {indexber} index The index of the piece in gameMatrix to move
      * @param {Element} div The DOM element reference to the piece
      */
-    function movePiece(index, div) {
-        if (!canMove(index)) {
+    function movePiece(gameMatrix, index, div) {
+        if (!canMove(gameMatrix, index)) {
             return;
         }
-        let selectedPosition = GAME_MATRIX[index];
-        let emptyPosition = GAME_MATRIX[GRID_DIM - 1];
+        let selectedPosition = gameMatrix[index];
+        let emptyPosition = gameMatrix[GRID_DIM - 1];
         let xOffset = 0;
         let yOffset = 0;
         if (selectedPosition.x === emptyPosition.x) {
@@ -51,9 +53,9 @@
             xOffset = selectedPosition.x - emptyPosition.x;
         }
         // Swap the position data
-        let temp = GAME_MATRIX[index];
-        GAME_MATRIX[index] = GAME_MATRIX[GRID_DIM - 1];
-        GAME_MATRIX[GRID_DIM - 1] = temp;
+        let temp = gameMatrix[index];
+        gameMatrix[index] = gameMatrix[GRID_DIM - 1];
+        gameMatrix[GRID_DIM - 1] = temp;
         //Move piece in the DOM
         if (yOffset !== 0) {
             div.style.top = (parseInt(div.style.top, 10) - yOffset * PUZZLE_DIM) + "px";
@@ -64,7 +66,7 @@
         // Win Check
         let win = true;
         for (let i = 0; i < GRID_DIM; i++) {
-            if (GAME_MATRIX[i].x !== i % NUM_COL || GAME_MATRIX[i].y !== Math.floor(i / NUM_ROW)) {
+            if (gameMatrix[i].x !== i % NUM_COL || gameMatrix[i].y !== Math.floor(i / NUM_ROW)) {
                 win = false;
                 break;
             }
@@ -72,21 +74,22 @@
         if (win) {
             let r = confirm("Hai vinto!\nVuoi fare un anuova partita?");
             if (r) {
-                shuffle();
+                shuffle(gameMatrix);
             }
         }
     }
 
     /**
      * Shuffle the puzzle
+     * @param {Position[]} gameMatrix The array containing pieces position
      */
-    function shuffle() {
+    function shuffle(gameMatrix) {
         for (let j = 0; j < NUM_SHUFFLE; j++) {
-            for (let i = 0; i < GAME_MATRIX.length; i++) {
+            for (let i = 0; i < gameMatrix.length; i++) {
                 if (i === PUZZLE_DIM - 1) {
                     continue;
                 }
-                if (canMove(i) && (Math.floor(Math.random() * RANDOM_VALUE) === 0)) {
+                if (canMove(gameMatrix, i) && (Math.floor(Math.random() * RANDOM_VALUE) === 0)) {
                     movePiece(i, document.getElementById("piece_" + i));
                 }
             }
@@ -97,14 +100,30 @@
      * Init the the document for the game.
      */
     window.addEventListener('DOMContentLoaded', function () {
-        document.getElementById("shufflebutton").onclick = shuffle;
         // Initiailize game engine
+
+
+        /**
+        * A position of a piece
+        * @typedef {Object} Position
+        * @property {number} x - X coordinate
+        * @property {number} y - Y coordinate
+        */
+
+
+        /** @type Position[] */
+        let gameMatrix = [GRID_DIM];
         for (let i = 0; i < GRID_DIM; i++) {
-            GAME_MATRIX[i] = {
+            gameMatrix[i] = {
                 x: i % NUM_COL,
                 y: Math.floor(i / NUM_ROW)
             };
         }
+
+        document.getElementById("shufflebutton").onclick = function () {
+            shuffle(gameMatrix);
+        };
+
         // Create puzzle pieces
         let puzzleArea = document.getElementById("puzzlearea");
         let divs = puzzleArea.getElementsByTagName("div");
@@ -120,7 +139,7 @@
             div.position = "absolute";
             div.id = "piece_" + index;
             div.onmouseenter = function () {
-                if (canMove(index)) {
+                if (canMove(gameMatrix, index)) {
                     div.classList.add("hover");
                 }
             };
@@ -128,7 +147,7 @@
                 div.classList.remove("hover");
             };
             div.onclick = function () {
-                movePiece(index, div);
+                movePiece(gameMatrix, index, div);
             };
         }
     });
