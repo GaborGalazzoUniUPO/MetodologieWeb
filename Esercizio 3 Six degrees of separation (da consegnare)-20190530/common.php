ad86350dcda112f0d2c4e92d754e5f88ee5a4402
Gabor Galazzo <gabor.galazzo@star-group.net>
2019-06-01 00:14:30 +0200

    Kevin Bacon OK

---
+++ Esercizio 3 Six degrees of separation (da consegnare)-20190530/common.php
@@ -0,0 +1,87 @@
+<?php
+    
+    /**
+     * @return PDO the connection to the database
+     */
+    function getConnection()
+    {
+        
+        static $connection;
+        
+        $db_user = 'root';
+        $db_password = '';
+        $db_name = 'imdb_small';
+        $db_host = 'localhost';
+        
+        if (!$connection) {
+            try {
+                $connection = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_password);
+                $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (Exception $e) {
+                echo "Database connection error: <br/>";
+                echo $e->getMessage();
+                exit(1);
+            }
+        }
+        
+        return $connection;
+    }
+    
+    /**
+     * @param $firstName string actor's first name
+     * @param $lastName  string actor's surname
+     *
+     * @return integer|null the matching actor's id
+     */
+    function findIdBestActorMatch($firstName, $lastName)
+    {
+        $query = "select id from actors
+        where first_name LIKE concat(:first_name , '%')
+        and last_name = :last_name
+        order by film_count desc, id
+        limit 1";
+        $stm = getConnection()->prepare($query);
+        $stm->execute(
+            [
+                'first_name' => $firstName,
+                'last_name' => $lastName,
+            ]
+        );
+        if ($result = $stm->fetchColumn()) {
+            return $result;
+        }
+        
+        return null;
+    }
+    
+    function createTable($table, $caption)
+    {
+        ?>
+
+        <table>
+            <caption><?= $caption ?></caption>
+            <tr>
+                <th>#</th>
+                <th>Title</th>
+                <th>Year</th>
+            </tr>
+            <?php
+                $number = 1;
+                foreach ($table as $line) {
+                    $title = htmlspecialchars($line["name"]);
+                    ?>
+                    <tr>
+                        <td><?=$number ?></td>
+                        <td><?=$title ?></td>
+                        <td><?=$line["year"] ?></td>
+                    </tr>
+                    <?php
+                    $number++;
+                }
+            ?>
+        </table>
+        <?php
+        
+    }
+    
+    
---
+++ Esercizio 3 Six degrees of separation (da consegnare)-20190530/search-all.php
@@ -0,0 +1,31 @@
+<?php include("top.html"); ?>
+
+<?php
+    
+    require_once "common.php";
+    
+    $actorId = findIdBestActorMatch($_REQUEST['firstname'], $_REQUEST['lastname']);
+    if ($actorId) {
+        $searchAllQuery = "select m.name, m.year from movies m
+        inner join roles r on r.movie_id = m.id
+        where r.actor_id = :actor_id";
+        $stm = getConnection()->prepare($searchAllQuery);
+        $stm->execute(
+            [
+                'actor_id' => $actorId,
+            ]
+        );
+        ?>
+        <h1>Results for <b><?= $_REQUEST['firstname']." ".$_REQUEST['lastname'] ?></b></h1>
+        <?php
+        createTable($stm->fetchAll(PDO::FETCH_ASSOC),
+            "All Films"
+            );
+    } else {
+        ?>
+        <h1>Actor <b> <?= $_REQUEST['firstname']." ".$_REQUEST['lastname'] ?> </b> not found</h1>
+        <?php
+    }
+
+?>
+<?php include("bottom.html"); ?>
