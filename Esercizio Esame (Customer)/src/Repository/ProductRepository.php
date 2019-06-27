c500ef41aa34c0db0d272c5cf50e0e9746dce8fe
Gabor Galazzo <gabor.galazzo@star-group.net>
2019-06-27 18:29:51 +0200

    Improoved search

--- Esercizio Esame (Customer)/src/Repository/ProductRepository.php
+++ Esercizio Esame (Customer)/src/Repository/ProductRepository.php
@@ -148,6 +148,8 @@ class ProductRepository extends AbstractRepository implements Repository
         $stm = $this->connection->prepare($query);
 
         $search_text = str_replace(' ','* ',preg_replace('/[+\-><\(\)~*\"@]+/', ' ', trim($search_text)))."*";
+        if(strlen($search_text)==1)
+            $search_text = "";
         $stm->bindParam('search_text',$search_text , PDO::PARAM_STR);
         $stm->bindParam('category', $category, PDO::PARAM_INT);
         $stm->bindParam('type', $type, PDO::PARAM_INT);
@@ -165,7 +167,7 @@ class ProductRepository extends AbstractRepository implements Repository
         select count(*) from (select p.id
         from products p
             left join reviews r on p.id = r.product_id
-        where (match(p.name, p.small_description, p.description, p.category_info) against (:search_text) || :search_text = '')
+        where (match(p.name, p.small_description, p.description, p.category_info) against (:search_text in boolean mode) || :search_text = '')
         and (p.date_added + interval 7 day > NOW() || 2 <> :type)
         and (p.category = :category || :category = 0)
         group by p.id
@@ -174,6 +176,8 @@ class ProductRepository extends AbstractRepository implements Repository
         $stm = $this->connection->prepare($query);
 
         $search_text = str_replace(' ','* ',preg_replace('/[+\-><\(\)~*\"@]+/', ' ', trim($search_text)))."*";
+        if(strlen($search_text)==1)
+            $search_text = "";
         $stm->bindParam('search_text',$search_text , PDO::PARAM_STR);
         $stm->bindParam('category', $category, PDO::PARAM_INT);
         $stm->bindParam('type', $type, PDO::PARAM_INT);
