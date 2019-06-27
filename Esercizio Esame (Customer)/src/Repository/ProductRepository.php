51d825e3ede08b5949279ab790f2579dfca6ae67
Gabor Galazzo <gabor.galazzo@star-group.net>
2019-06-27 18:18:47 +0200

    fixed search term

--- Esercizio Esame (Customer)/src/Repository/ProductRepository.php
+++ Esercizio Esame (Customer)/src/Repository/ProductRepository.php
@@ -136,7 +136,7 @@ class ProductRepository extends AbstractRepository implements Repository
             (select count(*) from stock_units s where s.product_id = p.id and s.status = 0) as stock_count
         from products p
             left join reviews r on p.id = r.product_id
-        where (match(p.name, p.small_description, p.description, p.category_info) against (:search_text) || :search_text = '')
+        where (match(p.name, p.small_description, p.description, p.category_info) against (:search_text in boolean mode) || :search_text = '')
         and (p.date_added + interval 7 day > NOW() || 2 <> :type)
         and (p.category = :category || :category = 0)
         group by p.id
@@ -147,7 +147,7 @@ class ProductRepository extends AbstractRepository implements Repository
 
         $stm = $this->connection->prepare($query);
 
-        $search_text = preg_replace('/[+\-><\(\)~*\"@]+/', ' ', $search_text);
+        $search_text = str_replace(' ','* ',preg_replace('/[+\-><\(\)~*\"@]+/', ' ', trim($search_text)))."*";
         $stm->bindParam('search_text',$search_text , PDO::PARAM_STR);
         $stm->bindParam('category', $category, PDO::PARAM_INT);
         $stm->bindParam('type', $type, PDO::PARAM_INT);
@@ -173,7 +173,7 @@ class ProductRepository extends AbstractRepository implements Repository
 
         $stm = $this->connection->prepare($query);
 
-        $search_text = preg_replace('/[+\-><\(\)~*\"@]+/', ' ', $search_text);
+        $search_text = str_replace(' ','* ',preg_replace('/[+\-><\(\)~*\"@]+/', ' ', trim($search_text)))."*";
         $stm->bindParam('search_text',$search_text , PDO::PARAM_STR);
         $stm->bindParam('category', $category, PDO::PARAM_INT);
         $stm->bindParam('type', $type, PDO::PARAM_INT);
